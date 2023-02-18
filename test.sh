3c38da6231000be8b0c3f5066ad9077abe8aaba2
Alik Kavkazov <kavkazov@hotmail.com>
2023-02-18 16:44:06 +0500

    change: test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ mkdir /etc/test/
 cp test.py /etc/test/test.py
 for ((i=1;i<=20;i++))
 do
-if scp "$HOME/test.py" "root@dp$i:/etc/test/test.py"
+if scp "$HOME/test-systemd/test.py" "root@dp$i:/etc/test/test.py"
 then
-if scp "/etc/systemd/system/test123.service" "root@dp$i:/etc/systemd/system/test123.service"
+if scp "$HOME/test-systemd/test123.service" "root@dp$i:/etc/systemd/system/test123.service"
 then
 ssh "root@dp$1" "systemctl start test123; systemctl enable test123"
 fi
