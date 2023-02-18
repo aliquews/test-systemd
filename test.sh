e2cc96ddb8d5e2823b3d5b8fc4c78521844a0ef7
Alik Kavkazov <kavkazov@hotmail.com>
2023-02-18 16:40:47 +0500

    initial

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir /etc/test/
+cp test.py /etc/test/test.py
+for ((i=1;i<=20;i++))
+do
+if scp "$HOME/test.py" "root@dp$i:/etc/test/test.py"
+then
+if scp "/etc/systemd/system/test123.service" "root@dp$i:/etc/systemd/system/test123.service"
+then
+ssh "root@dp$1" "systemctl start test123; systemctl enable test123"
+fi
+fi
+done
+
